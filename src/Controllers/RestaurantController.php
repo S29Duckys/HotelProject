d39f88c2fd44a4f12a747084ba280277f8cf3679
Noah7637 <benhabrou.n@gmail.com>
2026-03-17 12:04:36 +0100

    feat: implement RestaurantManager and update RestaurantController to fetch menus, enhance restaurant view

--- src/Controllers/RestaurantController.php
+++ src/Controllers/RestaurantController.php
@@ -2,16 +2,23 @@
 
 namespace MVC\Controllers;
 
+use MVC\Models\RestaurantManager;
+use MVC\Validator;
+
 class RestaurantController
 {
+    private $manager;
+    private $validator;
+
     public function __construct()
     {
-        $this->manager = new BarManager();
+        $this->manager = new RestaurantManager();
         $this->validator = new Validator();
     }
 
     public function index()
     {
+        $menus = $this->manager->getAll();
         require VIEWS . 'App/restaurant.php';
     }
 
