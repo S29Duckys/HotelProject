bea3886422046e2c9759690f312b8a5e7f48696e
Noah7637 <benhabrou.n@gmail.com>
2026-03-17 11:31:18 +0100

    feat: implement Restaurant model and manager, fix  BarController and RestaurantController (commandes working)

--- src/Models/BarManager.php
+++ src/Models/BarManager.php
@@ -45,9 +45,14 @@ class BarManager
     public function getClientBoisson()
     {
 
-        $stmt = $this->bdd->prepare("SELECT cb.id_client, cb.id_client_boisson, cb.id_boisson, cb.quantite AS client_boisson_qte, cb.date AS client_boisson_date, cl.nom AS client_nom, cl.prenom AS client_prenom, bo.name AS boisson_name, bo.prix_un AS prix_un_boisson FROM client_boisson cb JOIN client cl ON cb.id_client = cl.id_client JOIN boisson bo ON cb.id_boisson = bo.id_boisson ORDER BY cb.date DESC LIMIT 2");
+       try {
+        $stmt = $this->bdd->prepare("SELECT cb.id_boisson, cb.quantite AS client_boisson_qte, cb.date AS client_boisson_date, cl.nom AS client_nom, cl.prenom AS client_prenom, bo.name AS boisson_name, bo.prix_un AS prix_un_boisson FROM client_boisson cb JOIN client cl ON cb.id_client = cl.id_client JOIN boisson bo ON cb.id_boisson = bo.id_boisson ORDER BY cb.date DESC LIMIT 2");
+        $stmt->execute();
         $stmt->setFetchMode(\PDO::FETCH_CLASS, "MVC\Models\Bar");
-
+        
         return $stmt->fetchAll();
+       } catch (\Exception   $e) {
+        die($e->getMessage);
+       }
     }
 }
